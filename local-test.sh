939b5e9f5e18315be73a97d531ed35060458f11b
Pavel Shirshov <pshirshov@eml.cc>
2024-10-24 11:30:00 +0100

    test: gha

--- local-test.sh
+++ local-test.sh
@@ -3,6 +3,36 @@
 set -x
 set -e
 
-sbt GraalVMNativeImage/packageBin
+pushd .
 
-./local-test.sh
+target/graalvm-native-image/baboon \
+          --model-dir ./src/test/resources/baboon/ \
+          --output ./test/cs-stub/ConversionsTest/Generated \
+          --test-output ./test/cs-stub/ConversionsTest/Generated \
+          --cs-use-compact-adt-form true \
+          --cs-wrapped-adt-branch-codecs false
+
+cd ./test/cs-stub
+dotnet build
+dotnet test ConversionsTest/ConversionsTest.csproj
+
+popd
+
+pushd .
+
+target/graalvm-native-image/baboon \
+          --model-dir ./src/test/resources/baboon/ \
+          --output ./test/cs-stub/ConversionsTest/Generated \
+          --test-output ./test/cs-stub/ConversionsTest/Generated \
+          --cs-use-compact-adt-form false \
+          --cs-wrapped-adt-branch-codecs true
+
+
+# workaround for https://github.com/NixOS/nixpkgs/issues/350806
+# export PATH=`echo $PATH | tr ":" "\n" | grep -v "dotnet-runtime-6" | tr "\n" ":"`
+
+cd ./test/cs-stub
+dotnet build
+dotnet test ConversionsTest/ConversionsTest.csproj
+
+popd
