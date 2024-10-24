61e9b15515698c0d6cc076a05717ded5ac9770f5
Pavel Shirshov <pshirshov@eml.cc>
2024-10-24 19:52:15 +0100

    wip: gha test

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "baboon build environment";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
