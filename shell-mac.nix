a2aafec305d7682fd0205557be2a197c18f076df
Pavel Shirshov <pshirshov@eml.cc>
2024-10-24 11:20:00 +0100

    test: gha

--- shell-mac.nix
+++ shell-mac.nix
@@ -1,5 +1,5 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz") { } }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ ncurses sbt dotnet-sdk_7 darwin.CF ];
+  nativeBuildInputs = with pkgs.buildPackages; [ ncurses sbt dotnet-sdk_7 darwin.apple_sdk.frameworks.Foundation ];
 }
