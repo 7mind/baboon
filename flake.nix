4d47e9e5bf9dec5a468ca6ba97261405d3502530
Pavel Shirshov <pshirshov@eml.cc>
2025-01-08 17:38:27 +0000

    nix env/direnv

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
   description = "baboon build environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.11-beta";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
+  inputs.sbt.url = "github:zaninime/sbt-derivation";
+  inputs.sbt.inputs.nixpkgs.follows = "nixpkgs";
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
+    { self
+    , nixpkgs
+    , flake-utils
+    , sbt
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -17,35 +20,41 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages = rec {
+          baboon = sbt.lib.mkSbtDerivation {
+            pkgs = pkgs;
+            version = "0.0.89";
+            pname = "baboon";
+            src = ./.;
+            depsSha256 = "sha256-zkXAh5hPQVKYgG3g4Zj7Y8uDJ198698EFUUErxk0z5U=";
+            nativeBuildInputs = with pkgs; [
+              graalvm-ce
+            ];
+            depsWarmupCommand = ''
+              sbt update
+            '';
+            buildPhase = ''
+              ./build.sh build
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp target/graalvm-native-image/baboon $out/bin/baboon
+            '';
+          };
+          default = baboon;
+        };
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs.buildPackages; [
             ncurses
-            #graalvm-ce
-
-            # https://github.com/NixOS/nixpkgs/issues/350909
-            (graalvm-ce.overrideDerivation (oldAttrs: {
-
-              postInstall =
-                let
-                  darwinArgs = pkgs.lib.optionals stdenv.hostPlatform.isDarwin [
-                    "-ENIX_BINTOOLS"
-                    "-ENIX_CC"
-                    "-ENIX_CFLAGS_COMPILE"
-                    "-ENIX_LDFLAGS"
-                    "-ENIX_CC_WRAPPER_TARGET_HOST_${pkgs.stdenv.cc.suffixSalt}"
-                    "-ENIX_BINTOOLS_WRAPPER_TARGET_HOST_${pkgs.stdenv.cc.suffixSalt}"
-                  ];
-
-                  darwinFlags = (map (f: "--add-flags '${f}'") darwinArgs);
-                in
-
-                pkgs.lib.replaceStrings [ "/bin/native-image" ] [
-                  "/bin/native-image ${toString (darwinFlags)}"
-                ] oldAttrs.postInstall;
-            }))
-
-            sbt
+            gitMinimal
+            graalvm-ce
+            pkgs.sbt
             dotnet-sdk_8
+
+            coreutils
+            shellspec
+            nix
           ];
         };
       }
