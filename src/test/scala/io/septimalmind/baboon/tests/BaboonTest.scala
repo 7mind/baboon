c492c65ec2ddf28c91cf8ce106253b3b8af183fc
Kai <450507+neko-kai@users.noreply.github.com>
2025-01-20 20:45:42 +0000

    rename merge->getOrThrow

--- src/test/scala/io/septimalmind/baboon/tests/BaboonTest.scala
+++ src/test/scala/io/septimalmind/baboon/tests/BaboonTest.scala
@@ -60,7 +60,7 @@ object BaboonTest {
   private type FThrowable[F[+_, +_]] = { type l[+A] = F[Throwable, A] }
 
   private implicit final class EitherOps[A](private val value: Either[Throwable, A]) extends AnyVal {
-    def merge: A = value match {
+    def getOrThrow: A = value match {
       case Left(value)  => throw value
       case Right(value) => value
     }
@@ -69,22 +69,24 @@ object BaboonTest {
   private implicit val quasiAsync: QuasiAsync[FThrowable[Either]#l] = {
     val id = QuasiAsync.quasiAsyncIdentity
     new QuasiAsync[FThrowable[Either]#l] {
-      override def async[A](effect: (Either[Throwable, A] => Unit) => Unit): Either[Throwable, A]                           = Right(id.async[A](effect))
-      override def parTraverse[A, B](l: IterableOnce[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]]          = Right(id.parTraverse(l)(f(_).merge))
-      override def parTraverse_[A](l: IterableOnce[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]            = Right(id.parTraverse_(l)(f(_).merge))
-      override def parTraverseN[A, B](n: Int)(l: IterableOnce[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]] = Right(id.parTraverseN(n)(l)(f(_).merge))
-      override def parTraverseN_[A](n: Int)(l: IterableOnce[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]   = Right(id.parTraverseN_(n)(l)(f(_).merge))
+      override def async[A](effect: (Either[Throwable, A] => Unit) => Unit): Either[Throwable, A]                  = Right(id.async[A](effect))
+      override def parTraverse[A, B](l: IterableOnce[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]] = Right(id.parTraverse(l)(f(_).getOrThrow))
+      override def parTraverse_[A](l: IterableOnce[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]   = Right(id.parTraverse_(l)(f(_).getOrThrow))
+      override def parTraverseN[A, B](n: Int)(l: IterableOnce[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]] = Right(
+        id.parTraverseN(n)(l)(f(_).getOrThrow)
+      )
+      override def parTraverseN_[A](n: Int)(l: IterableOnce[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit] = Right(id.parTraverseN_(n)(l)(f(_).getOrThrow))
     }
   }
 
   private implicit val quasiIO: QuasiIO[FThrowable[Either]#l] = {
     val id = QuasiIO.quasiIOIdentity
     new QuasiIO[FThrowable[Either]#l] {
-      override def flatMap[A, B](fa: Either[Throwable, A])(f: A => Either[Throwable, B]): Either[Throwable, B] = id.flatMap(fa.merge)(f)
+      override def flatMap[A, B](fa: Either[Throwable, A])(f: A => Either[Throwable, B]): Either[Throwable, B] = id.flatMap(fa.getOrThrow)(f)
       override def guaranteeOnFailure[A](fa: => Either[Throwable, A])(cleanupOnFailure: Throwable => Either[Throwable, Unit]): Either[Throwable, A] =
-        id.guaranteeOnFailure(fa)(cleanupOnFailure(_).merge)
+        id.guaranteeOnFailure(fa)(cleanupOnFailure(_).getOrThrow)
       override def bracketCase[A, B](acquire: => Either[Throwable, A])(release: (A, Option[Throwable]) => Either[Throwable, Unit])(use: A => Either[Throwable, B])
-        : Either[Throwable, B] = Right(id.bracketCase[A, B](acquire.merge)((a: A, o: Option[Throwable]) => release(a, o).merge)(use(_).merge))
+        : Either[Throwable, B] = Right(id.bracketCase[A, B](acquire.getOrThrow)((a: A, o: Option[Throwable]) => release(a, o).getOrThrow)(use(_).getOrThrow))
       override def maybeSuspend[A](eff: => A): Either[Throwable, A]                          = Right(id.maybeSuspend[A](eff))
       override def maybeSuspendEither[A](eff: => Either[Throwable, A]): Either[Throwable, A] = Right(id.maybeSuspendEither[A](eff))
       override def definitelyRecoverUnsafeIgnoreTrace[A](action: => Either[Throwable, A])(recover: Throwable => Either[Throwable, A]): Either[Throwable, A] =
@@ -92,18 +94,18 @@ object BaboonTest {
       override def definitelyRecoverWithTrace[A](action: => Either[Throwable, A])(recoverWithTrace: (Throwable, Exit.Trace[Throwable]) => Either[Throwable, A])
         : Either[Throwable, A] = id.definitelyRecoverWithTrace(action)(recoverWithTrace)
       override def redeem[A, B](action: => Either[Throwable, A])(failure: Throwable => Either[Throwable, B], success: A => Either[Throwable, B]): Either[Throwable, B] =
-        id.redeem(action.merge)(failure, success)
+        id.redeem(action.getOrThrow)(failure, success)
       override def fail[A](t: => Throwable): Either[Throwable, A]                                      = id.fail(t)
       override def suspendF[A](effAction: => Either[Throwable, A]): Either[Throwable, A]               = id.suspendF(effAction)
-      override def tailRecM[A, B](a: A)(f: A => Either[Throwable, Either[A, B]]): Either[Throwable, B] = Right(id.tailRecM[A, B](a)(f(_).merge))
+      override def tailRecM[A, B](a: A)(f: A => Either[Throwable, Either[A, B]]): Either[Throwable, B] = Right(id.tailRecM[A, B](a)(f(_).getOrThrow))
       override def bracket[A, B](acquire: => Either[Throwable, A])(release: A => Either[Throwable, Unit])(use: A => Either[Throwable, B]): Either[Throwable, B] =
-        Right(id.bracket[A, B](acquire.merge)(release(_).merge)(use(_).merge))
-      override def guarantee[A](fa: => Either[Throwable, A])(`finally`: => Either[Throwable, Unit]): Either[Throwable, A] = id.guarantee(fa)(`finally`.merge)
-      override def traverse[A, B](l: Iterable[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]]               = Right(id.traverse(l)(f(_).merge))
-      override def traverse_[A](l: Iterable[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]                 = Right(id.traverse_(l)(f(_).merge))
+        Right(id.bracket[A, B](acquire.getOrThrow)(release(_).getOrThrow)(use(_).getOrThrow))
+      override def guarantee[A](fa: => Either[Throwable, A])(`finally`: => Either[Throwable, Unit]): Either[Throwable, A] = id.guarantee(fa)(`finally`.getOrThrow)
+      override def traverse[A, B](l: Iterable[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]]               = Right(id.traverse(l)(f(_).getOrThrow))
+      override def traverse_[A](l: Iterable[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]                 = Right(id.traverse_(l)(f(_).getOrThrow))
       override def pure[A](a: A): Either[Throwable, A]                                                                    = Right(a)
       override def map2[A, B, C](fa: Either[Throwable, A], fb: => Either[Throwable, B])(f: (A, B) => C): Either[Throwable, C] = Right(
-        id.map2[A, B, C](fa.merge, fb.merge)(f)
+        id.map2[A, B, C](fa.getOrThrow, fb.getOrThrow)(f)
       )
       override def map[A, B](fa: Either[Throwable, A])(f: A => B): Either[Throwable, B] = fa.map(f)
 
@@ -117,7 +119,7 @@ object BaboonTest {
   }
 
   private implicit val quasiIORunner: QuasiIORunner[FThrowable[Either]#l] = new QuasiIORunner[FThrowable[Either]#l] {
-    override def run[A](f: => Either[Throwable, A]): A = f.merge
+    override def run[A](f: => Either[Throwable, A]): A = f.getOrThrow
   }
 
   private val EitherDefaultModule: DefaultModule[FThrowable[Either]#l] = DefaultModule2(new ModuleDef {
