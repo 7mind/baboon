0c90f33dc3b347f35ff603bf307f56c229422a61
Kai <450507+neko-kai@users.noreply.github.com>
2025-01-20 20:07:58 +0000

    refactor baboon to use F-"abstract" tests instead of Identity/Either

--- src/test/scala/io/septimalmind/baboon/tests/BaboonTest.scala
+++ src/test/scala/io/septimalmind/baboon/tests/BaboonTest.scala
@@ -1,18 +1,24 @@
 package io.septimalmind.baboon.tests
 
 import distage.plugins.PluginBase
+import distage.{DefaultModule, ModuleDef}
 import io.septimalmind.baboon.*
+import io.septimalmind.baboon.tests.BaboonTest.BaboonTestModule
+import izumi.distage.modules.DefaultModule2
 import izumi.distage.plugins.PluginConfig
 import izumi.distage.testkit.model.TestConfig
-import izumi.distage.testkit.scalatest.Spec1
+import izumi.distage.testkit.scalatest.Spec2
+import izumi.functional.bio.Exit
+import izumi.functional.quasi.{QuasiAsync, QuasiIO, QuasiIORunner, QuasiRef}
 import izumi.fundamentals.platform.functional.Identity
+import izumi.reflect.TagKK
 
 import java.nio.file.Paths
 
-abstract class BaboonTest extends Spec1[Identity] {
+abstract class BaboonTest[F[+_, +_]: TagKK](implicit baboonTestModule: BaboonTestModule[F]) extends Spec2[F]()(baboonTestModule.defaultModule, implicitly[TagKK[F]]) {
   override protected def config: TestConfig = super.config.copy(
     pluginConfig = PluginConfig.const(
-      new BaboonModule(
+      new BaboonModule[Either](
         CompilerOptions(
           debug = false,
           target = TargetOptions(
@@ -41,4 +47,80 @@ abstract class BaboonTest extends Spec1[Identity] {
       ).morph[PluginBase]
     )
   )
+}
+
+object BaboonTest {
+  final class BaboonTestModule[F[+_, +_]](val defaultModule: DefaultModule2[F])
+  object BaboonTestModule {
+    implicit val eitherBaboonTestModule: BaboonTestModule[Either] = new BaboonTestModule[Either](EitherDefaultModule)
+  }
+
+  private type FThrowable[F[+_, +_]] = { type l[+A] = F[Throwable, A] }
+
+  private implicit final class EitherOps[A](private val value: Either[Throwable, A]) extends AnyVal {
+    def merge: A = value match {
+      case Left(value)  => throw value
+      case Right(value) => value
+    }
+  }
+
+  private implicit val quasiAsync: QuasiAsync[FThrowable[Either]#l] = {
+    val id = QuasiAsync.quasiAsyncIdentity
+    new QuasiAsync[FThrowable[Either]#l] {
+      override def async[A](effect: (Either[Throwable, A] => Unit) => Unit): Either[Throwable, A]                           = Right(id.async[A](effect))
+      override def parTraverse[A, B](l: IterableOnce[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]]          = Right(id.parTraverse(l)(f(_).merge))
+      override def parTraverse_[A](l: IterableOnce[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]            = Right(id.parTraverse_(l)(f(_).merge))
+      override def parTraverseN[A, B](n: Int)(l: IterableOnce[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]] = Right(id.parTraverseN(n)(l)(f(_).merge))
+      override def parTraverseN_[A](n: Int)(l: IterableOnce[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]   = Right(id.parTraverseN_(n)(l)(f(_).merge))
+    }
+  }
+
+  private implicit val quasiIO: QuasiIO[FThrowable[Either]#l] = {
+    val id = QuasiIO.quasiIOIdentity
+    new QuasiIO[FThrowable[Either]#l] {
+      override def flatMap[A, B](fa: Either[Throwable, A])(f: A => Either[Throwable, B]): Either[Throwable, B] = id.flatMap(fa.merge)(f)
+      override def guaranteeOnFailure[A](fa: => Either[Throwable, A])(cleanupOnFailure: Throwable => Either[Throwable, Unit]): Either[Throwable, A] =
+        id.guaranteeOnFailure(fa)(cleanupOnFailure(_).merge)
+      override def bracketCase[A, B](acquire: => Either[Throwable, A])(release: (A, Option[Throwable]) => Either[Throwable, Unit])(use: A => Either[Throwable, B])
+        : Either[Throwable, B] = Right(id.bracketCase[A, B](acquire.merge)((a: A, o: Option[Throwable]) => release(a, o).merge)(use(_).merge))
+      override def maybeSuspend[A](eff: => A): Either[Throwable, A]                          = Right(id.maybeSuspend[A](eff))
+      override def maybeSuspendEither[A](eff: => Either[Throwable, A]): Either[Throwable, A] = Right(id.maybeSuspendEither[A](eff))
+      override def definitelyRecoverUnsafeIgnoreTrace[A](action: => Either[Throwable, A])(recover: Throwable => Either[Throwable, A]): Either[Throwable, A] =
+        id.definitelyRecoverUnsafeIgnoreTrace(action)(recover)
+      override def definitelyRecoverWithTrace[A](action: => Either[Throwable, A])(recoverWithTrace: (Throwable, Exit.Trace[Throwable]) => Either[Throwable, A])
+        : Either[Throwable, A] = id.definitelyRecoverWithTrace(action)(recoverWithTrace)
+      override def redeem[A, B](action: => Either[Throwable, A])(failure: Throwable => Either[Throwable, B], success: A => Either[Throwable, B]): Either[Throwable, B] =
+        id.redeem(action.merge)(failure, success)
+      override def fail[A](t: => Throwable): Either[Throwable, A]                                      = id.fail(t)
+      override def suspendF[A](effAction: => Either[Throwable, A]): Either[Throwable, A]               = id.suspendF(effAction)
+      override def tailRecM[A, B](a: A)(f: A => Either[Throwable, Either[A, B]]): Either[Throwable, B] = Right(id.tailRecM[A, B](a)(f(_).merge))
+      override def bracket[A, B](acquire: => Either[Throwable, A])(release: A => Either[Throwable, Unit])(use: A => Either[Throwable, B]): Either[Throwable, B] =
+        Right(id.bracket[A, B](acquire.merge)(release(_).merge)(use(_).merge))
+      override def guarantee[A](fa: => Either[Throwable, A])(`finally`: => Either[Throwable, Unit]): Either[Throwable, A] = id.guarantee(fa)(`finally`.merge)
+      override def traverse[A, B](l: Iterable[A])(f: A => Either[Throwable, B]): Either[Throwable, List[B]]               = Right(id.traverse(l)(f(_).merge))
+      override def traverse_[A](l: Iterable[A])(f: A => Either[Throwable, Unit]): Either[Throwable, Unit]                 = Right(id.traverse_(l)(f(_).merge))
+      override def pure[A](a: A): Either[Throwable, A]                                                                    = Right(a)
+      override def map2[A, B, C](fa: Either[Throwable, A], fb: => Either[Throwable, B])(f: (A, B) => C): Either[Throwable, C] = Right(
+        id.map2[A, B, C](fa.merge, fb.merge)(f)
+      )
+      override def map[A, B](fa: Either[Throwable, A])(f: A => B): Either[Throwable, B] = fa.map(f)
+
+      override def mkRef[A](a: A): Either[Throwable, QuasiRef[FThrowable[Either]#l, A]] = Right(new QuasiRef[FThrowable[Either]#l, A] {
+        private final val idRef: QuasiRef[Identity, A]          = id.mkRef[A](a)
+        override def get: Either[Throwable, A]                  = Right(idRef.get)
+        override def set(a: A): Either[Throwable, Unit]         = Right(idRef.set(a))
+        override def update(f: A => A): Either[Throwable, Unit] = Right(idRef.update(f))
+      })
+    }
+  }
+
+  private implicit val quasiIORunner: QuasiIORunner[FThrowable[Either]#l] = new QuasiIORunner[FThrowable[Either]#l] {
+    override def run[A](f: => Either[Throwable, A]): A = f.merge
+  }
+
+  private val EitherDefaultModule: DefaultModule[FThrowable[Either]#l] = DefaultModule2(new ModuleDef {
+    addImplicit[QuasiIO[FThrowable[Either]#l]]
+    addImplicit[QuasiIORunner[FThrowable[Either]#l]]
+    addImplicit[QuasiAsync[FThrowable[Either]#l]]
+  })
 }
--- src/test/scala/io/septimalmind/baboon/tests/ParserTest.scala
+++ src/test/scala/io/septimalmind/baboon/tests/ParserTest.scala
@@ -1,58 +1,56 @@
 package io.septimalmind.baboon.tests
 
 import io.septimalmind.baboon.parser.BaboonParser
-import io.septimalmind.baboon.parser.model.FSPath
+import io.septimalmind.baboon.parser.model.{FSPath, RawDomain}
+import io.septimalmind.baboon.tests.BaboonTest.BaboonTestModule
+import izumi.functional.bio.Error2
 import izumi.fundamentals.collections.nonempty.NEString
+import izumi.reflect.TagKK
 
-class ParserTest extends BaboonTest {
+final class ParserTest extends ParserTestBase[Either]
+
+abstract class ParserTestBase[F[+_, +_]: Error2: TagKK: BaboonTestModule] extends BaboonTest[F] {
   "baboon parser" should {
     "parse sources" in {
-      (parser: BaboonParser) =>
-        val parsed = parser.parse(
-          BaboonParser.Input(
-            FSPath.parse(NEString.unsafeFrom("testfile.baboon")),
-            """/* test comment */
-              |model my.test.model
-              |
-              |version "1.2.3"
-              |
-              |enum E1 { A B C }
-              |
-              |enum E2 {
-              | A
-              | B
-              | C
-              |}
-              |
-              |root data D1 {
-              | f1: i08
-              | f2: i32
-              | f3: opt[str]
-              | f4: map[str, str]
-              |}
-              |
-              |adt A1 {
-              |  data B1 {
-              |     value: D1
-              |  }
-              |
-              |  data B1 {
-              |     value: E1
-              |  }
-              |}
-              |""".stripMargin,
-          )
+      (parser: BaboonParser[F]) =>
+        val input = BaboonParser.Input(
+          FSPath.parse(NEString.unsafeFrom("testfile.baboon")),
+          """/* test comment */
+            |model my.test.model
+            |
+            |version "1.2.3"
+            |
+            |enum E1 { A B C }
+            |
+            |enum E2 {
+            | A
+            | B
+            | C
+            |}
+            |
+            |root data D1 {
+            | f1: i08
+            | f2: i32
+            | f3: opt[str]
+            | f4: map[str, str]
+            |}
+            |
+            |adt A1 {
+            |  data B1 {
+            |     value: D1
+            |  }
+            |
+            |  data B1 {
+            |     value: E1
+            |  }
+            |}
+            |""".stripMargin,
         )
-//      parsed match {
-//        case Left(value) =>
-//          value match {
-//            case NEList(BaboonIssue.ParserFailed(fail)) =>
-//              println(fail.trace())
-//          }
-//        case Right(value) =>
-//          println(value)
-//      }
-        assert(parsed.isRight)
+
+        for {
+          parsed <- parser.parse(input)
+          _       = assert(parsed.isInstanceOf[RawDomain])
+        } yield ()
     }
   }
 }
