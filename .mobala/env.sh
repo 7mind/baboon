a1c125b8c91a37ba891f2727c4564a184f4ff29d
Pavel Shirshov <pshirshov@eml.cc>
2025-04-17 18:02:04 +0100

    wip

--- .mobala/env.sh
+++ .mobala/env.sh
@@ -24,23 +24,6 @@ _JAVA_OPTIONS+=" ${JAVA_OPTIONS_TAIL:-""}"
 _JAVA_OPTIONS="$(echo "${_JAVA_OPTIONS}" | grep -v '#' | tr '\n' ' ' | tr -s ' ')"
 #------------------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------------------
-# CI env
-export BUILD_NUMBER="${BUILD_NUMBER:-0}"
-export BUILD_BRANCH="${BUILD_BRANCH:-"$(git branch --show-current)"}"
-declare -a BRANCH_PARTS="(${BUILD_BRANCH//\// })"
-export SHORT_BRANCH=${BRANCH_PARTS[${#BRANCH_PARTS[@]}-1]}
-#------------------------------------------------------------------------------------------
-
-export DO_FMT="${DO_FMT:-0}"
-export DO_BUILD="${DO_BUILD:-0}"
-export DO_TEST="${DO_TEST:-0}"
-export DO_MKDIST="${DO_MKDIST:-0}"
-#  export MKDIST_SOURCE=./target/graalvm-native-image
-#  export MKDIST_TARGET=./target/dist
-export DO_FLAKE_REFRESH="${DO_FLAKE_REFRESH:-0}"
-export DO_FLAKE_VALIDATE="${DO_FLAKE_VALIDATE:-0}"
-
 if [[ "${DO_VERBOSE}" == 1 && "${VERBOSE_LEVEL}" -gt 1 ]] ; then
   environment=$(env)
   environment=$(echo "$environment" | grep -v '^\s*$' | sed "s/^/[verbose:env] /;s/$/ /")
