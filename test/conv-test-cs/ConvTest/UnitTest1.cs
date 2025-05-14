0bf338ba47985ec6b9a1ac8cbd0f7eff69d255cc
Alex Liubymov <arowcap@gmail.com>
2025-05-14 16:36:46 +0300

    Bug: map[TK, lst[T]] conversion.

--- test/conv-test-cs/ConvTest/UnitTest1.cs
+++ test/conv-test-cs/ConvTest/UnitTest1.cs
@@ -6,8 +6,8 @@ namespace ConvTest
 {
     public class RequiredConversionsImpl : RequiredConversions
     {
-        
     }
+
     public class Tests
     {
         [SetUp]
@@ -21,22 +21,25 @@ namespace ConvTest
             var a1 = new Convtest.Testpkg.v1_0_0.Adt0.B1("val1");
 
             var conv = new BaboonConversions(new RequiredConversionsImpl());
-            
+
             var a1u1 = conv.Convert(a1).To<Adt0.B1>();
             Assert.That(a1.F == ((Adt0.B1)a1u1).F);
-            
+
             var a1u2 = conv.Convert<Convtest.Testpkg.v1_0_0.Adt0>(a1).To<Adt0>();
             Assert.That(a1u1 == a1u2);
-            
         }
-        
+
         [Test]
         public void Test_Opt_List_Auto_Upgrade()
         {
-            var t1 = new Convtest.Testpkg.v1_0_0.TransferOpt(Guid.Empty, new List<Convtest.Testpkg.v1_0_0.Adt0>{new Convtest.Testpkg.v1_0_0.Adt0.B1("val1")});
+            var t1 = new Convtest.Testpkg.v1_0_0.TransferOpt(
+                Guid.Empty,
+                new List<Convtest.Testpkg.v1_0_0.Adt0> { new Convtest.Testpkg.v1_0_0.Adt0.B1("val1") },
+                new Dictionary<string, IReadOnlyList<Convtest.Testpkg.v1_0_0.Adt0>> { { "1", new List<Convtest.Testpkg.v1_0_0.Adt0> { new Convtest.Testpkg.v1_0_0.Adt0.B1("val2") } } }
+            );
 
             var conv = new BaboonConversions(new RequiredConversionsImpl());
-            
+
             var t1u = conv.Convert(t1).To<TransferOpt>();
             Assert.That(t1.U == t1u.U);
         }
