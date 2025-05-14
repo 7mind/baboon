62ef996b6946b743f078a44c50e67bf4bb377c28
Alex Liubymov <arowcap@gmail.com>
2025-05-14 16:28:39 +0300

    Bug: opt[lst[T]] conversion.

--- test/conv-test-cs/ConvTest/UnitTest1.cs
+++ test/conv-test-cs/ConvTest/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using Convtest.Testpkg;
+﻿using System;
+using System.Collections.Generic;
+using Convtest.Testpkg;
 
 namespace ConvTest
 {
@@ -26,6 +28,17 @@ namespace ConvTest
             var a1u2 = conv.Convert<Convtest.Testpkg.v1_0_0.Adt0>(a1).To<Adt0>();
             Assert.That(a1u1 == a1u2);
             
+        }
+        
+        [Test]
+        public void Test_Opt_List_Auto_Upgrade()
+        {
+            var t1 = new Convtest.Testpkg.v1_0_0.TransferOpt(Guid.Empty, new List<Convtest.Testpkg.v1_0_0.Adt0>{new Convtest.Testpkg.v1_0_0.Adt0.B1("val1")});
+
+            var conv = new BaboonConversions(new RequiredConversionsImpl());
+            
+            var t1u = conv.Convert(t1).To<TransferOpt>();
+            Assert.That(t1.U == t1u.U);
         }
     }
 }
