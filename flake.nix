ecb9bef1d51ae400a9814bac758ace08a977a2ff
Pavel Shirshov <pshirshov@eml.cc>
2025-07-04 16:21:32 +0100

    Setting version to 0.0.126

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         packages = rec {
           baboon = sbt.lib.mkSbtDerivation {
             pkgs = pkgs;
-            version = "0.0.125";
+            version = "0.0.126";
             pname = "baboon";
             src = ./.;
             depsSha256 = "sha256-JmVPgZxhLWNfvG1HLjkZ4qBLUlrREBMMvOu9X+3mjfk=";
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := "0.0.126-SNAPSHOT"
+ThisBuild / version := "0.0.126"
