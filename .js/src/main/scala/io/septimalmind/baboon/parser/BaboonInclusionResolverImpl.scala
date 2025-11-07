3f07f485c19cae76fa54377fbdc4f28b83db1a2d
Pavel Shirshov <pshirshov@eml.cc>
2025-11-07 18:34:16 +0000

    wip

--- .js/src/main/scala/io/septimalmind/baboon/parser/BaboonInclusionResolverImpl.scala
+++ .js/src/main/scala/io/septimalmind/baboon/parser/BaboonInclusionResolverImpl.scala
@@ -1,14 +1,33 @@
 package io.septimalmind.baboon.parser
 
 import io.septimalmind.baboon.CompilerOptions
-import io.septimalmind.baboon.parser.model.RawInclude
+import io.septimalmind.baboon.parser.model.{FSPath, RawInclude}
 import izumi.functional.bio.Error2
+import izumi.fundamentals.collections.nonempty.NEString
 
-class BaboonInclusionResolverImpl[F[+_, +_]: Error2](
+class BaboonInclusionResolverImpl[F[+_, +_]](
   options: CompilerOptions,
   inputs: Seq[BaboonParser.Input],
 ) extends BaboonInclusionResolver[F] {
+
+  private val inputsCache: Map[FSPath, String] = inputs.map(i => i.path -> i.content).toMap
+
+  private def resolvePath(baseDir: FSPath, relativePath: String): FSPath = {
+    val relativeSegments   = relativePath.split("/").toSeq.filter(_.nonEmpty)
+    val relativeNESegments = relativeSegments.map(NEString.unsafeFrom)
+
+    baseDir match {
+      case FSPath.Full(location, name) =>
+        FSPath((location :+ name) ++ relativeNESegments)
+      case FSPath.Name(name) =>
+        FSPath(Seq(name) ++ relativeNESegments)
+    }
+  }
+
   def getIclusionContent(inc: RawInclude): Option[String] = {
-    None
+    options.directoryInputs.iterator
+      .map(dir => resolvePath(dir, inc.value))
+      .flatMap(resolvedPath => inputsCache.get(resolvedPath))
+      .nextOption()
   }
 }
