99de58591db8d9d7e12c4a4649e63b5f998d614f
Pavel Shirshov <pshirshov@eml.cc>
2025-11-07 18:52:50 +0000

    wip

--- baboon-compiler/.js/.js/src/main/scala/izumi/fundamentals/platform/files/IzFiles.scala
+++
@@ -1,9 +0,0 @@
-//package izumi.fundamentals.platform.files
-//
-//import java.io.File
-//
-//object IzFiles {
-//  def readString(file: File): String = {
-//    throw new UnsupportedOperationException("File I/O not supported in JavaScript environment")
-//  }
-//}
