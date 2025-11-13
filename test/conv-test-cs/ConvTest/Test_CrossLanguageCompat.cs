cc5a4c9e9330871ec86cba06ef824b9a098d3fa8
Pavel Shirshov <pshirshov@eml.cc>
2025-11-13 20:32:31 +0000

    dedup

--- test/conv-test-cs/ConvTest/Test_CrossLanguageCompat.cs
+++ test/conv-test-cs/ConvTest/Test_CrossLanguageCompat.cs
@@ -12,148 +12,89 @@ namespace ConvTest
     public class Test_CrossLanguageCompat
     {
         private readonly string baseDir = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "..", "..", "target", "compat-test"));
-        private string scalaJsonFile;
-        private string scalaUebaFile;
-        private string csJsonFile;
-        private string csUebaFile;
+        private readonly BaboonCodecContext ctx = BaboonCodecContext.Default;
 
-        [SetUp]
-        public void Setup()
+        // Helper methods
+        private AllBasicTypes ReadJsonFile(string source, string format)
         {
-            scalaJsonFile = Path.Combine(baseDir, "scala-json", "all-basic-types.json");
-            scalaUebaFile = Path.Combine(baseDir, "scala-ueba", "all-basic-types.ueba");
-            csJsonFile = Path.Combine(baseDir, "cs-json", "all-basic-types.json");
-            csUebaFile = Path.Combine(baseDir, "cs-ueba", "all-basic-types.ueba");
+            var file = Path.Combine(baseDir, $"{source}-json", "all-basic-types.json");
+            var jsonStr = File.ReadAllText(file, Encoding.UTF8);
+            using var reader = new JsonTextReader(new StringReader(jsonStr)) { DateParseHandling = DateParseHandling.None };
+            var jsonToken = JToken.Load(reader);
+            return AllBasicTypes_JsonCodec.Instance.Decode(ctx, jsonToken);
         }
 
+        private AllBasicTypes ReadUebaFile(string source, string format)
+        {
+            var file = Path.Combine(baseDir, $"{source}-ueba", "all-basic-types.ueba");
+            var uebaBytes = File.ReadAllBytes(file);
+            using var memoryStream = new MemoryStream(uebaBytes);
+            using var reader = new BinaryReader(memoryStream);
+            return AllBasicTypes_UEBACodec.Instance.Decode(ctx, reader);
+        }
+
+        private void AssertBasicFields(AllBasicTypes data, string label)
+        {
+            Console.WriteLine($"Successfully decoded {label}: {data.Vstr}");
+            Assert.That(data.Vstr, Is.EqualTo("Hello, Baboon!"));
+            Assert.That(data.Vi32, Is.EqualTo(123456));
+            Assert.That(data.Vbit, Is.True);
+        }
+
+        private void PrintComparison(string label, AllBasicTypes scalaData, AllBasicTypes csData)
+        {
+            Console.WriteLine($"Comparing Scala and C# {label} data:");
+            Console.WriteLine($"  Scala: vi8={scalaData.Vi8}, vi16={scalaData.Vi16}, vi32={scalaData.Vi32}, vi64={scalaData.Vi64}");
+            Console.WriteLine($"  C#:    vi8={csData.Vi8}, vi16={csData.Vi16}, vi32={csData.Vi32}, vi64={csData.Vi64}");
+            Console.WriteLine($"  Scala: vf32={scalaData.Vf32}, vf64={scalaData.Vf64}, vf128={scalaData.Vf128}");
+            Console.WriteLine($"  C#:    vf32={csData.Vf32}, vf64={csData.Vf64}, vf128={csData.Vf128}");
+            Console.WriteLine($"  Scala: vtsu={scalaData.Vtsu}, vtso={scalaData.Vtso}");
+            Console.WriteLine($"  C#:    vtsu={csData.Vtsu}, vtso={csData.Vtso}");
+        }
+
+        // JSON Tests
         [Test]
         public void CSharp_JSON_Deserialization_Should_Read_Scala_Generated_JSON()
         {
-            var jsonStr = File.ReadAllText(scalaJsonFile, Encoding.UTF8);
-            using var reader = new JsonTextReader(new StringReader(jsonStr)) { DateParseHandling = DateParseHandling.None };
-            var jsonToken = JToken.Load(reader);
-
-            var ctx = BaboonCodecContext.Default;
-            var decoded = AllBasicTypes_JsonCodec.Instance.Decode(ctx, jsonToken);
-
-            Console.WriteLine($"Successfully decoded Scala JSON: {decoded.Vstr}");
-            Assert.That(decoded.Vstr, Is.EqualTo("Hello, Baboon!"));
-            Assert.That(decoded.Vi32, Is.EqualTo(123456));
-            Assert.That(decoded.Vbit, Is.True);
+            AssertBasicFields(ReadJsonFile("scala", "Scala JSON"), "Scala JSON");
         }
 
         [Test]
         public void CSharp_JSON_Deserialization_Should_Read_CSharp_Generated_JSON()
         {
-            var jsonStr = File.ReadAllText(csJsonFile, Encoding.UTF8);
-            using var reader = new JsonTextReader(new StringReader(jsonStr)) { DateParseHandling = DateParseHandling.None };
-            var jsonToken = JToken.Load(reader);
-
-            var ctx = BaboonCodecContext.Default;
-            var decoded = AllBasicTypes_JsonCodec.Instance.Decode(ctx, jsonToken);
-
-            Console.WriteLine($"Successfully decoded C# JSON: {decoded.Vstr}");
-            Assert.That(decoded.Vstr, Is.EqualTo("Hello, Baboon!"));
-            Assert.That(decoded.Vi32, Is.EqualTo(123456));
-            Assert.That(decoded.Vbit, Is.True);
+            AssertBasicFields(ReadJsonFile("cs", "C# JSON"), "C# JSON");
         }
 
+        // UEBA Tests
         [Test]
         public void CSharp_UEBA_Deserialization_Should_Read_Scala_Generated_UEBA()
         {
-            var uebaBytes = File.ReadAllBytes(scalaUebaFile);
-            using (var memoryStream = new MemoryStream(uebaBytes))
-            using (var reader = new BinaryReader(memoryStream))
-            {
-                var ctx = BaboonCodecContext.Default;
-                var decoded = AllBasicTypes_UEBACodec.Instance.Decode(ctx, reader);
-
-                Console.WriteLine($"Successfully decoded Scala UEBA: {decoded.Vstr}");
-                Assert.That(decoded.Vstr, Is.EqualTo("Hello, Baboon!"));
-                Assert.That(decoded.Vi32, Is.EqualTo(123456));
-                Assert.That(decoded.Vbit, Is.True);
-            }
+            AssertBasicFields(ReadUebaFile("scala", "Scala UEBA"), "Scala UEBA");
         }
 
         [Test]
         public void CSharp_UEBA_Deserialization_Should_Read_CSharp_Generated_UEBA()
         {
-            var uebaBytes = File.ReadAllBytes(csUebaFile);
-            using (var memoryStream = new MemoryStream(uebaBytes))
-            using (var reader = new BinaryReader(memoryStream))
-            {
-                var ctx = BaboonCodecContext.Default;
-                var decoded = AllBasicTypes_UEBACodec.Instance.Decode(ctx, reader);
-
-                Console.WriteLine($"Successfully decoded C# UEBA: {decoded.Vstr}");
-                Assert.That(decoded.Vstr, Is.EqualTo("Hello, Baboon!"));
-                Assert.That(decoded.Vi32, Is.EqualTo(123456));
-                Assert.That(decoded.Vbit, Is.True);
-            }
+            AssertBasicFields(ReadUebaFile("cs", "C# UEBA"), "C# UEBA");
         }
 
+        // Cross-language comparison
         [Test]
         public void CrossLanguage_Comparison_Should_Verify_Scala_And_CSharp_JSON_Produce_Equivalent_Data()
         {
-            var scalaJsonStr = File.ReadAllText(scalaJsonFile, Encoding.UTF8);
-            JToken scalaJson;
-            using (var reader = new JsonTextReader(new StringReader(scalaJsonStr)) { DateParseHandling = DateParseHandling.None })
-            {
-                scalaJson = JToken.Load(reader);
-            }
-
-            var csJsonStr = File.ReadAllText(csJsonFile, Encoding.UTF8);
-            JToken csJson;
-            using (var reader = new JsonTextReader(new StringReader(csJsonStr)) { DateParseHandling = DateParseHandling.None })
-            {
-                csJson = JToken.Load(reader);
-            }
-
-            var ctx = BaboonCodecContext.Default;
-            var scalaDecoded = AllBasicTypes_JsonCodec.Instance.Decode(ctx, scalaJson);
-            var csDecoded = AllBasicTypes_JsonCodec.Instance.Decode(ctx, csJson);
-
-            Console.WriteLine("Comparing Scala and C# JSON data:");
-            Console.WriteLine($"  Scala: vi8={scalaDecoded.Vi8}, vi16={scalaDecoded.Vi16}, vi32={scalaDecoded.Vi32}, vi64={scalaDecoded.Vi64}");
-            Console.WriteLine($"  C#:    vi8={csDecoded.Vi8}, vi16={csDecoded.Vi16}, vi32={csDecoded.Vi32}, vi64={csDecoded.Vi64}");
-            Console.WriteLine($"  Scala: vf32={scalaDecoded.Vf32}, vf64={scalaDecoded.Vf64}, vf128={scalaDecoded.Vf128}");
-            Console.WriteLine($"  C#:    vf32={csDecoded.Vf32}, vf64={csDecoded.Vf64}, vf128={csDecoded.Vf128}");
-            Console.WriteLine($"  Scala: vtsu={scalaDecoded.Vtsu}, vtso={scalaDecoded.Vtso}");
-            Console.WriteLine($"  C#:    vtsu={csDecoded.Vtsu}, vtso={csDecoded.Vtso}");
-
-            Assert.That(csDecoded, Is.EqualTo(scalaDecoded), "Scala and C# JSON data should be equal");
+            var scalaData = ReadJsonFile("scala", "Scala JSON");
+            var csData = ReadJsonFile("cs", "C# JSON");
+            PrintComparison("JSON", scalaData, csData);
+            Assert.That(csData, Is.EqualTo(scalaData), "Scala and C# JSON data should be equal");
         }
 
         [Test]
         public void CrossLanguage_Comparison_Should_Verify_Scala_And_CSharp_UEBA_Produce_Equivalent_Data()
         {
-            var scalaUebaBytes = File.ReadAllBytes(scalaUebaFile);
-            AllBasicTypes scalaDecoded;
-            using (var memoryStream = new MemoryStream(scalaUebaBytes))
-            using (var reader = new BinaryReader(memoryStream))
-            {
-                var ctx = BaboonCodecContext.Default;
-                scalaDecoded = AllBasicTypes_UEBACodec.Instance.Decode(ctx, reader);
-            }
-
-            var csUebaBytes = File.ReadAllBytes(csUebaFile);
-            AllBasicTypes csDecoded;
-            using (var memoryStream = new MemoryStream(csUebaBytes))
-            using (var reader = new BinaryReader(memoryStream))
-            {
-                var ctx = BaboonCodecContext.Default;
-                csDecoded = AllBasicTypes_UEBACodec.Instance.Decode(ctx, reader);
-            }
-
-            Console.WriteLine("Comparing Scala and C# UEBA data:");
-            Console.WriteLine($"  Scala: vi8={scalaDecoded.Vi8}, vi16={scalaDecoded.Vi16}, vi32={scalaDecoded.Vi32}, vi64={scalaDecoded.Vi64}");
-            Console.WriteLine($"  C#:    vi8={csDecoded.Vi8}, vi16={csDecoded.Vi16}, vi32={csDecoded.Vi32}, vi64={csDecoded.Vi64}");
-            Console.WriteLine($"  Scala: vf32={scalaDecoded.Vf32}, vf64={scalaDecoded.Vf64}, vf128={scalaDecoded.Vf128}");
-            Console.WriteLine($"  C#:    vf32={csDecoded.Vf32}, vf64={csDecoded.Vf64}, vf128={csDecoded.Vf128}");
-            Console.WriteLine($"  Scala: vtsu={scalaDecoded.Vtsu}, vtso={scalaDecoded.Vtso}");
-            Console.WriteLine($"  C#:    vtsu={csDecoded.Vtsu}, vtso={csDecoded.Vtso}");
-
-            Assert.That(csDecoded, Is.EqualTo(scalaDecoded), "Scala and C# UEBA data should be equal");
+            var scalaData = ReadUebaFile("scala", "Scala UEBA");
+            var csData = ReadUebaFile("cs", "C# UEBA");
+            PrintComparison("UEBA", scalaData, csData);
+            Assert.That(csData, Is.EqualTo(scalaData), "Scala and C# UEBA data should be equal");
         }
     }
 }
--- test/conv-test-sc/src/test/scala/example/Test_CrossLanguageCompat.scala
+++ test/conv-test-sc/src/test/scala/example/Test_CrossLanguageCompat.scala
@@ -1,140 +1,87 @@
 package example
 
 import convtest.testpkg.{AllBasicTypes, AllBasicTypes_JsonCodec, AllBasicTypes_UEBACodec}
-import baboon.runtime.shared.LEDataInputStream
+import baboon.runtime.shared.{BaboonCodecContext, LEDataInputStream}
 import org.scalatest.flatspec.AnyFlatSpec
-import java.io.{File, FileInputStream}
+import java.io.FileInputStream
 import java.nio.file.{Files, Paths}
 import java.nio.charset.StandardCharsets
 import io.circe.parser.parse
 
 class Test_CrossLanguageCompat extends AnyFlatSpec {
 
-  val baseDir = Paths.get("../../target/compat-test").toAbsolutePath.normalize()
-  val scalaJsonFile = baseDir.resolve("scala-json/all-basic-types.json")
-  val scalaUebaFile = baseDir.resolve("scala-ueba/all-basic-types.ueba")
-  val csJsonFile = baseDir.resolve("cs-json/all-basic-types.json")
-  val csUebaFile = baseDir.resolve("cs-ueba/all-basic-types.ueba")
+  private val baseDir = Paths.get("../../target/compat-test").toAbsolutePath.normalize()
+  private val ctx = BaboonCodecContext.Default
 
+  // Helper methods
+  private def readJsonFile(source: String, format: String): AllBasicTypes = {
+    val file = baseDir.resolve(s"$source-json/all-basic-types.json")
+    val jsonStr = new String(Files.readAllBytes(file), StandardCharsets.UTF_8)
+    val json = parse(jsonStr).getOrElse(fail(s"Failed to parse $format JSON from $file"))
+    AllBasicTypes_JsonCodec.instance.decode(ctx, json) match {
+      case Right(data) => data
+      case Left(error) => fail(s"Failed to decode $format JSON: $error")
+    }
+  }
+
+  private def readUebaFile(source: String, format: String): AllBasicTypes = {
+    val file = baseDir.resolve(s"$source-ueba/all-basic-types.ueba")
+    val fis = new FileInputStream(file.toFile)
+    try {
+      val reader = new LEDataInputStream(fis)
+      AllBasicTypes_UEBACodec.instance.decode(ctx, reader)
+    } finally {
+      fis.close()
+    }
+  }
+
+  private def assertBasicFields(data: AllBasicTypes, label: String): Unit = {
+    println(s"Successfully decoded $label: ${data.vstr}")
+    assert(data.vstr == "Hello, Baboon!")
+    assert(data.vi32 == 123456)
+    assert(data.vbit)
+  }
+
+  private def printComparison(label: String, scala: AllBasicTypes, cs: AllBasicTypes): Unit = {
+    println(s"Comparing Scala and C# $label data:")
+    println(s"  Scala: vi8=${scala.vi8}, vi16=${scala.vi16}, vi32=${scala.vi32}, vi64=${scala.vi64}")
+    println(s"  C#:    vi8=${cs.vi8}, vi16=${cs.vi16}, vi32=${cs.vi32}, vi64=${cs.vi64}")
+    println(s"  Scala: vf32=${scala.vf32}, vf64=${scala.vf64}, vf128=${scala.vf128}")
+    println(s"  C#:    vf32=${cs.vf32}, vf64=${cs.vf64}, vf128=${cs.vf128}")
+    println(s"  Scala: vtsu=${scala.vtsu}, vtso=${scala.vtso}")
+    println(s"  C#:    vtsu=${cs.vtsu}, vtso=${cs.vtso}")
+  }
+
+  // JSON Tests
   "Scala JSON deserialization" should "read Scala-generated JSON" in {
-    val jsonStr = new String(Files.readAllBytes(scalaJsonFile), StandardCharsets.UTF_8)
-    val json = parse(jsonStr).getOrElse(fail(s"Failed to parse JSON from $scalaJsonFile"))
-
-    val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-    val decoded = AllBasicTypes_JsonCodec.instance.decode(ctx, json)
-
-    decoded match {
-      case Right(data) =>
-        println(s"Successfully decoded Scala JSON: ${data.vstr}")
-        assert(data.vstr == "Hello, Baboon!")
-        assert(data.vi32 == 123456)
-        assert(data.vbit)
-      case Left(error) =>
-        fail(s"Failed to decode Scala JSON: $error")
-    }
+    assertBasicFields(readJsonFile("scala", "Scala JSON"), "Scala JSON")
   }
 
-  "Scala JSON deserialization" should "read C#-generated JSON" in {
-    val jsonStr = new String(Files.readAllBytes(csJsonFile), StandardCharsets.UTF_8)
-    val json = parse(jsonStr).getOrElse(fail(s"Failed to parse JSON from $csJsonFile"))
-
-    val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-    val decoded = AllBasicTypes_JsonCodec.instance.decode(ctx, json)
-
-    decoded match {
-      case Right(data) =>
-        println(s"Successfully decoded C# JSON: ${data.vstr}")
-        assert(data.vstr == "Hello, Baboon!")
-        assert(data.vi32 == 123456)
-        assert(data.vbit)
-      case Left(error) =>
-        fail(s"Failed to decode C# JSON: $error")
-    }
+  it should "read C#-generated JSON" in {
+    assertBasicFields(readJsonFile("cs", "C# JSON"), "C# JSON")
   }
 
+  // UEBA Tests
   "Scala UEBA deserialization" should "read Scala-generated UEBA" in {
-    val uebaBytes = Files.readAllBytes(scalaUebaFile)
-    val fis = new FileInputStream(scalaUebaFile.toFile)
-    try {
-      val reader = new LEDataInputStream(fis)
-      val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-      val decoded = AllBasicTypes_UEBACodec.instance.decode(ctx, reader)
-
-      println(s"Successfully decoded Scala UEBA: ${decoded.vstr}")
-      assert(decoded.vstr == "Hello, Baboon!")
-      assert(decoded.vi32 == 123456)
-      assert(decoded.vbit)
-    } finally {
-      fis.close()
-    }
+    assertBasicFields(readUebaFile("scala", "Scala UEBA"), "Scala UEBA")
   }
 
-  "Scala UEBA deserialization" should "read C#-generated UEBA" in {
-    val uebaBytes = Files.readAllBytes(csUebaFile)
-    val fis = new FileInputStream(csUebaFile.toFile)
-    try {
-      val reader = new LEDataInputStream(fis)
-      val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-      val decoded = AllBasicTypes_UEBACodec.instance.decode(ctx, reader)
-
-      println(s"Successfully decoded C# UEBA: ${decoded.vstr}")
-      assert(decoded.vstr == "Hello, Baboon!")
-      assert(decoded.vi32 == 123456)
-      assert(decoded.vbit)
-    } finally {
-      fis.close()
-    }
+  it should "read C#-generated UEBA" in {
+    assertBasicFields(readUebaFile("cs", "C# UEBA"), "C# UEBA")
   }
 
+  // Cross-language comparison
   "Cross-language comparison" should "verify Scala and C# JSON produce equivalent data" in {
-    val scalaJsonStr = new String(Files.readAllBytes(scalaJsonFile), StandardCharsets.UTF_8)
-    val scalaJson = parse(scalaJsonStr).getOrElse(fail("Failed to parse Scala JSON"))
-
-    val csJsonStr = new String(Files.readAllBytes(csJsonFile), StandardCharsets.UTF_8)
-    val csJson = parse(csJsonStr).getOrElse(fail("Failed to parse C# JSON"))
-
-    val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-    val scalaDecoded = AllBasicTypes_JsonCodec.instance.decode(ctx, scalaJson).getOrElse(fail("Failed to decode Scala JSON"))
-    val csDecoded = AllBasicTypes_JsonCodec.instance.decode(ctx, csJson).getOrElse(fail("Failed to decode C# JSON"))
-
-    println(s"Comparing Scala and C# JSON data:")
-    println(s"  Scala: vi8=${scalaDecoded.vi8}, vi16=${scalaDecoded.vi16}, vi32=${scalaDecoded.vi32}, vi64=${scalaDecoded.vi64}")
-    println(s"  C#:    vi8=${csDecoded.vi8}, vi16=${csDecoded.vi16}, vi32=${csDecoded.vi32}, vi64=${csDecoded.vi64}")
-    println(s"  Scala: vf32=${scalaDecoded.vf32}, vf64=${scalaDecoded.vf64}, vf128=${scalaDecoded.vf128}")
-    println(s"  C#:    vf32=${csDecoded.vf32}, vf64=${csDecoded.vf64}, vf128=${csDecoded.vf128}")
-    println(s"  Scala: vtsu=${scalaDecoded.vtsu}, vtso=${scalaDecoded.vtso}")
-    println(s"  C#:    vtsu=${csDecoded.vtsu}, vtso=${csDecoded.vtso}")
-
-    assert(scalaDecoded == csDecoded, "Scala and C# JSON data should be equal")
+    val scalaData = readJsonFile("scala", "Scala JSON")
+    val csData = readJsonFile("cs", "C# JSON")
+    printComparison("JSON", scalaData, csData)
+    assert(scalaData == csData, "Scala and C# JSON data should be equal")
   }
 
-  "Cross-language comparison" should "verify Scala and C# UEBA produce equivalent data" in {
-    val scalaFis = new FileInputStream(scalaUebaFile.toFile)
-    val scalaDecoded = try {
-      val reader = new LEDataInputStream(scalaFis)
-      val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-      AllBasicTypes_UEBACodec.instance.decode(ctx, reader)
-    } finally {
-      scalaFis.close()
-    }
-
-    val csFis = new FileInputStream(csUebaFile.toFile)
-    val csDecoded = try {
-      val reader = new LEDataInputStream(csFis)
-      val ctx = baboon.runtime.shared.BaboonCodecContext.Default
-      AllBasicTypes_UEBACodec.instance.decode(ctx, reader)
-    } finally {
-      csFis.close()
-    }
-
-    println(s"Comparing Scala and C# UEBA data:")
-    println(s"  Scala: vi8=${scalaDecoded.vi8}, vi16=${scalaDecoded.vi16}, vi32=${scalaDecoded.vi32}, vi64=${scalaDecoded.vi64}")
-    println(s"  C#:    vi8=${csDecoded.vi8}, vi16=${csDecoded.vi16}, vi32=${csDecoded.vi32}, vi64=${csDecoded.vi64}")
-    println(s"  Scala: vf32=${scalaDecoded.vf32}, vf64=${scalaDecoded.vf64}, vf128=${scalaDecoded.vf128}")
-    println(s"  C#:    vf32=${csDecoded.vf32}, vf64=${csDecoded.vf64}, vf128=${csDecoded.vf128}")
-    println(s"  Scala: vtsu=${scalaDecoded.vtsu}, vtso=${scalaDecoded.vtso}")
-    println(s"  C#:    vtsu=${csDecoded.vtsu}, vtso=${csDecoded.vtso}")
-
-    assert(scalaDecoded == csDecoded, "Scala and C# UEBA data should be equal")
+  it should "verify Scala and C# UEBA produce equivalent data" in {
+    val scalaData = readUebaFile("scala", "Scala UEBA")
+    val csData = readUebaFile("cs", "C# UEBA")
+    printComparison("UEBA", scalaData, csData)
+    assert(scalaData == csData, "Scala and C# UEBA data should be equal")
   }
 }
