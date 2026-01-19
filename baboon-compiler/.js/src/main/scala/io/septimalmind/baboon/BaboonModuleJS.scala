2bc8d1289169c3ba375073b3a60147bd65d283ab
Pavel Shirshov <pshirshov@eml.cc>
2026-01-19 19:11:58 +0000

    fixes

--- baboon-compiler/.js/src/main/scala/io/septimalmind/baboon/BaboonModuleJS.scala
+++ baboon-compiler/.js/src/main/scala/io/septimalmind/baboon/BaboonModuleJS.scala
@@ -1,10 +1,26 @@
 package io.septimalmind.baboon
 
 import distage.{ModuleDef, TagKK}
+import io.septimalmind.baboon.parser.model.FSPath
 import io.septimalmind.baboon.parser.{BaboonInclusionResolver, BaboonInclusionResolverMapImpl, BaboonParser}
-import io.septimalmind.baboon.util.BLogger
+import io.septimalmind.baboon.util.{BLogger, FileContentProvider}
 import izumi.functional.bio.unsafe.MaybeSuspend2
 import izumi.functional.bio.{Error2, ParallelErrorAccumulatingOps2}
+
+final class FileContentProviderFromInputs(inputs: Seq[BaboonParser.Input]) extends FileContentProvider {
+  private val contentsByPath: Map[FSPath, String] =
+    inputs.map(input => input.path -> input.content).toMap
+
+  override def read(path: FSPath): Option[String] = {
+    contentsByPath.get(path)
+  }
+}
+
+final class FileContentProviderUnsupported extends FileContentProvider {
+  override def read(path: FSPath): Option[String] = {
+    throw new IllegalStateException(s"FileContentProvider is unavailable without inputs: ${path.asString}")
+  }
+}
 
 class BaboonModuleJS[F[+_, +_]: Error2: MaybeSuspend2: TagKK](
   inputs: Seq[BaboonParser.Input],
@@ -17,6 +33,7 @@ class BaboonModuleJS[F[+_, +_]: Error2: MaybeSuspend2: TagKK](
 
   make[Seq[BaboonParser.Input]].fromValue(inputs)
   make[CompilerOptions].fromValue(compilerOptions)
+  make[FileContentProvider].fromValue(new FileContentProviderFromInputs(inputs))
 
   make[BaboonInclusionResolver[F]].from[BaboonInclusionResolverMapImpl[F]]
   make[BaboonLoaderJS[F]].from[BaboonLoaderJS.BaboonLoaderJSImpl[F]]
@@ -38,4 +55,5 @@ class BaboonCodecModuleJS[F[+_, +_]: Error2: MaybeSuspend2: TagKK](
   parOps: ParallelErrorAccumulatingOps2[F]
 ) extends ModuleDef {
   include(new BaboonModuleLogicModule[F](parOps))
+  make[FileContentProvider].fromValue(new FileContentProviderUnsupported)
 }
